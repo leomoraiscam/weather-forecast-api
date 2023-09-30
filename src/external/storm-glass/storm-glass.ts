dd2f95d161b450ecdfe604f1218ee09d86807a03
lmorais <lmorais.contato@gmail.com>
2023-09-30 18:29:14 -0300

    feat: add axios to call storm glass api

--- src/external/storm-glass/storm-glass.ts
+++ src/external/storm-glass/storm-glass.ts
@@ -1,5 +1,20 @@
+import { AxiosStatic } from "axios";
+
 export class StormGlass {
+  constructor(protected request: AxiosStatic) {}
+ 
+  readonly stormGlassAPIParams =
+    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+  readonly stormGlassAPISource = 'noaa';
+
   public async fetchPoints(lat: number, long: number): Promise<{}> {
-    return Promise.resolve({})
+    const response = await this.request.get(`https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&lat=${lat}&lng=${long}`,
+    {
+      headers: {
+        Authorization: 'fake-token',
+      },
+    });
+
+    return response;
   }
 }
