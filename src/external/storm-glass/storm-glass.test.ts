81e843057c981e4cfed540bad067d9d9b9b41b47
lmorais <lmorais.contato@gmail.com>
2023-09-30 18:50:19 -0300

    test: use fixture in storm glass response api mocks

--- src/external/storm-glass/storm-glass.test.ts
+++ src/external/storm-glass/storm-glass.test.ts
@@ -1,4 +1,5 @@
 import { StormGlass } from './storm-glass';
+import stormGlassWeather3HoursFixture  from "@test/fixtures/storm-glass-weather-3-hours.json";
 import axios from "axios";
 
 jest.mock('axios');
@@ -10,12 +11,12 @@ describe('Storm Glass Client', () => {
     const lat = -33.792726;
     const long = 151.289824;
 
-    axios.get = mockAxiosGet.mockResolvedValue({});
+    axios.get = mockAxiosGet.mockResolvedValue(stormGlassWeather3HoursFixture);
 
     const stormGlass = new StormGlass(axios);
 
     const response = await stormGlass.fetchPoints(lat, long);
 
-    expect(response).toEqual({});
+    expect(response).toEqual(stormGlassWeather3HoursFixture);
   });
 });
