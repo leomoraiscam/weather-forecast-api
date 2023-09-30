dae424c72013f5d5874d289ae12164656092465c
lmorais <lmorais.contato@gmail.com>
2023-09-30 17:19:45 -0300

    chore: add configuration jest to unit tests

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { resolve } = require('path');
+
+const root = resolve(__dirname);
+
+module.exports = {
+  rootDir: root,
+  displayName: 'root-tests',
+  testMatch: ['<rootDir>/src/modules/**/*.test.ts'],
+  testEnvironment: 'node',
+  clearMocks: true,
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@test/(.*)': '<rootDir>/tests/$1',
+  },
+};
