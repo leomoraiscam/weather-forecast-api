df8820f4d5f92e60fe0f7fed260318ea26b30302
lmorais <lmorais.contato@gmail.com>
2023-10-08 00:44:06 -0300

    test: add Beach Domain Model

---
+++ src/modules/forecast/domain/beach/beach.test.ts
@@ -0,0 +1,24 @@
+import { BeachPosition } from '@config/constants/beach-position-enum'
+import { Beach } from './beach'
+import { Name } from "./name";
+import { Position } from "./position";
+import { Latitude } from "./latitude";
+import { Longitude } from "./longitude";
+
+describe('Domain Beach model', () => {
+  it('should be able to create new beach', () => {
+    const name = Name.create('Dee Why') as Name;
+    const lat = Latitude.create( -33.792726) as Latitude;
+    const lng = Longitude.create(141.289824) as Longitude;
+    const position = Position.create(BeachPosition.S) as Position;
+    
+    const beach = Beach.create({
+      name,
+      lat,
+      lng,
+      position,
+    })
+
+    expect(beach).toHaveProperty('_id');
+  })
+})
