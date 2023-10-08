e328c04e0599b731492cf672f81e9737454024f1
lmorais <lmorais.contato@gmail.com>
2023-10-07 23:33:58 -0300

    test: add Beach Name Object Value

---
+++ src/modules/forecast/domain/beach/name.test.ts
@@ -0,0 +1,18 @@
+import { Name } from "./name";
+import { InvalidNameError } from "./errors/invalid-name-error"
+
+describe('Beach Name Object Value', () => {
+  it('should accept valid name', () => {
+    const nameOrError = Name.create('John Doe');
+
+    expect(nameOrError).toHaveProperty('name');
+  })
+
+  it('should reject name with less than 2 characters', () => {
+    expect(() => Name.create('j')).toThrow(InvalidNameError);
+  })
+
+  it('should reject name with more than 255 characters', () => {
+    expect(() => Name.create('d'.repeat(260))).toThrow(InvalidNameError);
+  })
+})
