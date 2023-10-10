1711a59afafa53e02fbf06f3c16ca2f4f15d3f6d
lmorais <lmorais.contato@gmail.com>
2023-10-10 15:33:43 -0300

    feat: add express server

---
+++ src/main/app.ts
@@ -0,0 +1,17 @@
+import '../utils/module-alias'
+import express from 'express';
+import setupMiddleware from './middleware';
+import setupRoute from './config/routes';
+
+const app = express();
+
+setupMiddleware(app);
+setupRoute(app);
+
+app.listen(process.env.APP_PORT || 3000, () => {
+  console.log(
+    `Server running at http://localhost:${process.env.APP_PORT || 3000}`
+  );
+});
+
+export default app;
