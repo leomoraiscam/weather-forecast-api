2c5465d76b7625dc054ad02dbe9953a7f563e5e4
lmorais <lmorais.contato@gmail.com>
2023-10-10 15:33:27 -0300

    feat: add default middleware to make request to express

---
+++ src/main/middleware/content-type.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const contentType = (
+  _: Request,
+  response: Response,
+  next: NextFunction
+): void => {
+  response.type('json');
+
+  next();
+};
