e5455ec09f43bcd8670ff1ebae2de1b8c0ce3853
lmorais <lmorais.contato@gmail.com>
2023-10-18 22:33:10 -0300

    chore: add new depedencies in factory file

--- src/main/factories/fetch-points.ts
+++ src/main/factories/fetch-points.ts
@@ -1,8 +1,13 @@
 import { ProcessForecastBeachesUseCase } from "@src/modules/forecast/usecases/processForecastForBeaches/process-forecast-for-beaches-use-case"
 import { FetchPointsController } from "@src/modules/forecast/usecases/processForecastForBeaches/process-forecast-for-beaches-controller";
+import { AxiosRequestProvider } from "@src/external/stormglass-service/providers/implementations/axios-request-provider";
+import { FetchPointService } from  "@src/external/stormglass-service/fetch-point-service"
 
 export const makeFetchPointController = (): FetchPointsController => {
-  const processForecastBeachesUseCase = new ProcessForecastBeachesUseCase();
+  const axiosRequestProvider = new AxiosRequestProvider();
+  const stormGlassService = new FetchPointService(axiosRequestProvider)
+
+  const processForecastBeachesUseCase = new ProcessForecastBeachesUseCase(stormGlassService);
   const fetchPointsController = new FetchPointsController(processForecastBeachesUseCase);
 
   return fetchPointsController;
