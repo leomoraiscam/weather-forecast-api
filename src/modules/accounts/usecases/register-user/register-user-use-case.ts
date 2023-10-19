f7c2af71b3ab3f02241760eb2be9169d0c70d823
lmorais <lmorais.contato@gmail.com>
2023-10-19 19:17:50 -0300

    feat: add register user use case

---
+++ src/modules/accounts/usecases/register-user/register-user-use-case.ts
@@ -0,0 +1,40 @@
+import { Email } from "../../domain/user/email";
+import { Name } from "../../domain/user/name";
+import { Password } from "../../domain/user/password";
+import { User } from "../../domain/user/user";
+import { IUsersRepository } from "../../repositories/users-repository";
+import { AccountAlreadyExistsError } from "./errors/AccountAlreadyExistsError";
+
+export class RegisterUserUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  async execute({
+    name,
+    email,
+    password,
+  }: { name: string, email: string, password: string }): Promise<User> {
+    const nameOrError = Name.create(name) as Name
+    const emailOrError = Email.create(email) as Email
+    const passwordOrError = Password.create(password) as Password
+
+    const userOrError = User.create({
+      name: nameOrError,
+      email: emailOrError,
+      password: passwordOrError,
+    }) as User
+    
+    const user = userOrError.props
+
+    const userAlreadyExists = await this.usersRepository.findByEmail(
+      user.email.value
+    )
+
+    if (userAlreadyExists) {
+      throw new AccountAlreadyExistsError(user.email.value)
+    }
+
+    await this.usersRepository.create(userOrError)
+
+    return userOrError
+  }
+}
