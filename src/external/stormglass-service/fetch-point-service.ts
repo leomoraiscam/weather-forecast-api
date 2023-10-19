39b01b4e14de8f2e460e367f0bb10a981badb6dc
lmorais <lmorais.contato@gmail.com>
2023-10-18 22:29:25 -0300

    refactor: use axios provider by depedency injection

--- src/external/stormglass-service/fetch-point-service.ts
+++ src/external/stormglass-service/fetch-point-service.ts
@@ -1,4 +1,3 @@
-import * as HTTPUtil from "@src/utils/request"
 import { FetchPointNormalize } from "./ports/dtos/fetch-point-normalize";
 import { StormGlassForecastResponse } from "./ports/dtos/stormglass-response";
 import { StormGlassMapper } from "./mapper/stormglass-mapper"
@@ -6,26 +5,29 @@ import { FetchPointCoordinate } from "./ports/dtos/fetch-point-coordinate";
 import { StormGlassService } from "./ports/stormglass-service";
 import { StormGlassResponseError } from "./errors/stormglass-response-error";
 import { ClientRequestError } from "./errors/client-request-error";
+import { IRequestProvider } from "./providers/models/request-provider"
 
 export class FetchPointService implements StormGlassService {
-  constructor(protected request = new HTTPUtil.Request()) {}
- 
+  constructor(private requestProvider: IRequestProvider) {}
+
   readonly stormGlassAPIParams =
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
   readonly stormGlassAPISource = 'noaa';
 
   public async execute({ lat, long }: FetchPointCoordinate): Promise<FetchPointNormalize[]> {
     try {
-      const response = await this.request.get<StormGlassForecastResponse>(`https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&lat=${lat}&lng=${long}`,
-      {
-        headers: {
-          Authorization: 'fake-token',
-        },
+      const response = await this.requestProvider.get<StormGlassForecastResponse>({
+        url: `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&lat=${lat}&lng=${long}`,
+        config:       {
+          headers: {
+            Authorization: 'fake-token',
+          }
+        }
       });
 
       return StormGlassMapper.toNormalize(response.data);
     } catch (err) {
-      if (HTTPUtil.Request.isRequestError(err)) {
+      if (err.response && err.response.status) {
         throw new StormGlassResponseError(
           `Error: ${JSON.stringify(err.response.data)} Code: ${
             err.response.status
