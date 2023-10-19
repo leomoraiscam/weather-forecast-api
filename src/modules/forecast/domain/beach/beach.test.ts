142a80350f95231809285fe977267913038167e0
lmorais <lmorais.contato@gmail.com>
2023-10-18 22:32:34 -0300

    fix: change wrong data

--- src/modules/forecast/domain/beach/beach.test.ts
+++ src/modules/forecast/domain/beach/beach.test.ts
@@ -9,7 +9,7 @@ describe('Domain Beach model', () => {
   it('should be able to create new beach', () => {
     const name = Name.create('Dee Why') as Name;
     const lat = Latitude.create( -33.792726) as Latitude;
-    const lng = Longitude.create(141.289824) as Longitude;
+    const lng = Longitude.create(151.289824) as Longitude;
     const position = Position.create(BeachPosition.S) as Position;
     
     const beach = Beach.create({
