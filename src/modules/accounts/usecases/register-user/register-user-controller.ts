37772a6c389dbf0ac1f976c8286845aa9fba0b68
lmorais <lmorais.contato@gmail.com>
2023-10-21 01:12:23 -0300

    feat: add correct type and implementation validation request data

--- src/modules/accounts/usecases/register-user/register-user-controller.ts
+++ src/modules/accounts/usecases/register-user/register-user-controller.ts
@@ -1,9 +1,11 @@
 import { HttpResponse  } from "../../../forecast/usecases/ports/http/http-response";
 import { UseCase  } from "../../../forecast/usecases/ports/use-case";
 import { ControllerError } from "../../../forecast/usecases/errors/controller-error"
-import { created, serverError } from "../../../forecast/usecases/helper/http-helper";
+import { badRequest, created, serverError } from "../../../forecast/usecases/helper/http-helper";
 import { HttpRequest } from "../../../forecast/usecases/ports/http/http-request"
-import { User } from "../../domain/user/user";
+import { RegisterUserResponse } from "../../dtos/register-user-response";
+import { RegisterUserRequest } from "../../dtos/register-user";
+import { MissingParamError } from "@src/modules/forecast/usecases/errors/missing-param-error";
 
 export class RegisterUserController {
   private readonly usecase: UseCase;
@@ -13,12 +15,22 @@ export class RegisterUserController {
   }
 
   async handle(
-    request:HttpRequest<any>
-  ): Promise<HttpResponse<User | ControllerError>> {
+    request:HttpRequest<RegisterUserRequest>
+  ): Promise<HttpResponse<RegisterUserResponse | ControllerError>> {
     try {
+      const { body } = request;
+
+      if (!body?.name || !body?.email || !body?.password) {
+        const missing = !body?.name ? 'name' : !body?.email ? 'email' : 'password';
+
+        return badRequest<ControllerError>(
+          new MissingParamError(missing.trim())
+        );
+      } 
+
       const response = await this.usecase.execute(request.body);
 
-      return created<User>(response);
+      return created<RegisterUserResponse>(response);
     } catch (error) {
       return serverError<ControllerError>(error);
     }
