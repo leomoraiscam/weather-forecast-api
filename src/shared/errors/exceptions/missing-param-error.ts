5dbcf16d56542f8d1f190c5e1510d00ca393eefd
lmorais <lmorais.contato@gmail.com>
2023-10-23 15:26:32 -0300

    refactor: change MissingParamError

--- src/shared/errors/exceptions/missing-param-error.ts
+++ src/shared/errors/exceptions/missing-param-error.ts
@@ -1,6 +1,3 @@
-import { InternalError } from '@src/shared/errors/exceptions/internal-server-error';
-import { ControllerError } from '../ports/controller-error';
-
 export class MissingParamError extends Error{
   constructor(param: string) {
     super(`Missing parameter from request: ${param}.`);
