fda0752163caf458e0a8f30376ee805a4e0d05bb
lmorais <lmorais.contato@gmail.com>
2023-10-23 15:26:20 -0300

    refactor: change MissingParamError

--- src/shared/errors/exceptions/missing-param-error.ts
+++ src/shared/errors/exceptions/missing-param-error.ts
@@ -1,7 +1,7 @@
 import { InternalError } from '@src/shared/errors/exceptions/internal-server-error';
 import { ControllerError } from '../ports/controller-error';
 
-export class MissingParamError extends InternalError implements ControllerError {
+export class MissingParamError extends Error{
   constructor(param: string) {
     super(`Missing parameter from request: ${param}.`);
   }
