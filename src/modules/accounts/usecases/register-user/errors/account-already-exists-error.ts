f7fb033a54fdfabbd10a123c54734bf69b0334dd
lmorais <lmorais.contato@gmail.com>
2023-10-23 15:28:19 -0300

    feat: add use case error

--- src/modules/accounts/usecases/register-user/errors/account-already-exists-error.ts
+++ src/modules/accounts/usecases/register-user/errors/account-already-exists-error.ts
@@ -1,4 +1,6 @@
-export class AccountAlreadyExistsError extends Error {
+import { UseCaseError } from "@src/shared/errors/ports/use-case-error"
+
+export class AccountAlreadyExistsError extends Error implements UseCaseError {
   constructor(email: string) {
     super(`The email "${email}" is already registered.`)
     this.name = 'AccountAlreadyExistsError'
