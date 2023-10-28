f810d0b844843c248808197de3064a20422eb56b
lmorais <lmorais.contato@gmail.com>
2023-10-28 00:56:27 -0300

    refactor: change current error to custom app errors extends

--- src/external/stormglass-service/errors/stormglass-response-error.ts
+++ src/external/stormglass-service/errors/stormglass-response-error.ts
@@ -1,9 +1,8 @@
-import { InternalError } from "@src/shared/errors/exceptions/internal-server-error";
-
-export class StormGlassResponseError extends InternalError {
+export class StormGlassResponseError extends Error {
   constructor(message: string) {
     const internalMessage =
       'Unexpected error returned by the StormGlass service';
-    super(`${internalMessage}: ${message}`);
+      
+    super(`${internalMessage}${message}`);
   }
 }
