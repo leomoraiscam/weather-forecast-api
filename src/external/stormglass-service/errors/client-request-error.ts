d9b7ac97c666a805ba6a1010cf66e4f9d6b285a6
lmorais <lmorais.contato@gmail.com>
2023-11-03 09:56:18 -0300

    chore: remove oldest object values

--- src/external/stormglass-service/errors/client-request-error.ts
+++
@@ -1,8 +0,0 @@
-import { InternalError } from "@src/shared/errors/exceptions/internal-server-error";
-
-export class ClientRequestError extends InternalError {
-  constructor(message: string) {
-    const internalMessage = `Unexpected error when trying to communicate to StormGlass`;
-    super(`${internalMessage}: ${message}`)
-  }
-}
--- src/shared/errors/exceptions/missing-param-error.ts
+++
@@ -1,5 +0,0 @@
-export class MissingParamError extends Error{
-  constructor(param: string) {
-    super(`Missing parameter from request: ${param}.`);
-  }
-}
