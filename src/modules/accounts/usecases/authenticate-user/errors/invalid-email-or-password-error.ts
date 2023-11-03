a2afbaceaf6ad0dfd8dfb3f4873fdce204ed80f4
lmorais <lmorais.contato@gmail.com>
2023-11-03 09:58:33 -0300

    refactor: use error to use case

--- src/modules/accounts/usecases/authenticate-user/errors/invalid-email-or-password-error.ts
+++ src/modules/accounts/usecases/authenticate-user/errors/invalid-email-or-password-error.ts
@@ -1,4 +1,6 @@
-export class InvalidEmailOrPasswordError extends Error {
+import { UseCaseError } from "@src/shared/errors/ports/use-case-error"
+
+export class InvalidEmailOrPasswordError extends Error implements UseCaseError {
   constructor() {
     super(`Invalid e-mail/password combination.`)
     this.name = 'InvalidEmailOrPasswordError'
