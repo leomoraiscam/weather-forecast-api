a05b6dffb0ed6c12d1d41949adb25abe810346dd
lmorais <lmorais.contato@gmail.com>
2023-11-10 00:37:01 -0300

    feat: add configuration to db in app server

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,17 +1,28 @@
 import '../shared/module-alias'
 import express from 'express';
+import * as dotenv from 'dotenv';
+import { mongoHelper } from '@src/external/repositories/mongodb/helpers/mongo-helper';
 import setupMiddleware from './middleware';
 import setupRoute from './config/routes';
+
+dotenv.config();
 
 const app = express();
 
 setupMiddleware(app);
 setupRoute(app);
 
-app.listen(process.env.APP_PORT || 3000, () => {
-  console.log(
-    `Server running at http://localhost:${process.env.APP_PORT || 3000}`
-  );
-});
+mongoHelper
+  .connect(`mongodb://localhost:16017`)
+  .then(async () => {
+    app.listen(process.env.APP_PORT || 3000, () => {
+      console.log(
+        `Server running at http://localhost:${process.env.APP_PORT || 3000}`
+      );
+    });
+  })
+  .catch((error: any) => {
+    console.log(`Error: ${error}`);
+  });
 
 export default app;
