d4618d183712600f6570804aac449f606db881f4
lmorais <lmorais.contato@gmail.com>
2023-11-09 23:59:51 -0300

    chore: cleaning user mapper class methods

--- src/modules/accounts/mapper/user-mapper.ts
+++ src/modules/accounts/mapper/user-mapper.ts
@@ -1,12 +1,1 @@
-import { User } from "../domain/user/user";
-import { RegisterUser } from "../dtos/register-user-response";
-
-export class UserMapper {
-  static toDomain({ id , props: { name, email } }: User): RegisterUser {
-    return {
-      id,
-      name: name.value,
-      email: email.value,
-    }
-  }
-}
+export class UserMapper {}
