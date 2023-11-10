2e8bac632f1dd483032c513c8c7a9b4c7a77035d
lmorais <lmorais.contato@gmail.com>
2023-11-10 00:35:35 -0300

    feat: add a new implementation class to save data in repository in register user factory

--- src/main/factories/register-user.ts
+++ src/main/factories/register-user.ts
@@ -1,11 +1,11 @@
 import { RegisterUserUseCase } from "@src/modules/accounts/usecases/register-user/register-user-use-case"
 import { RegisterUserController } from "@src/modules/accounts/usecases/register-user/register-user-controller";
-import { InMemoryUsersRepository } from "@src/modules/accounts/repositories/in-memory/in-memory-users-repository"
+import { UserRepository } from "@src/modules/accounts/repositories/implementations/users-repository"
 import { Controller } from "../ports/controller";
 
 export const makeRegisterUserController = (): Controller => {
-  const inMemoryUsersRepository = new InMemoryUsersRepository()
-  const registerUserUseCase = new RegisterUserUseCase(inMemoryUsersRepository);
+  const userRepository = new UserRepository()
+  const registerUserUseCase = new RegisterUserUseCase(userRepository);
   const registerUserController = new RegisterUserController(registerUserUseCase);
 
   return registerUserController;
