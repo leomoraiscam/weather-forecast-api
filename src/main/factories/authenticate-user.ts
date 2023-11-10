54c69aa89553d16b6ba5aba99eb5e0899c14c3b9
lmorais <lmorais.contato@gmail.com>
2023-11-10 00:41:19 -0300

    feat: add a new implementation class to save data in repository in authenticate user factory

--- src/main/factories/authenticate-user.ts
+++ src/main/factories/authenticate-user.ts
@@ -1,11 +1,11 @@
 import { AuthenticateUser } from "@src/modules/accounts/usecases/authenticate-user/authenticate-user"
 import { AuthenticateUserController } from "@src/modules/accounts/usecases/authenticate-user/authenticate-user-controller";
-import { InMemoryUsersRepository } from "@src/modules/accounts/repositories/in-memory/in-memory-users-repository"
+import { UserRepository } from "@src/modules/accounts/repositories/implementations/users-repository"
 import { Controller } from "../ports/controller";
 
 export const makeAuthenticateUserController = (): Controller => {
-  const inMemoryUsersRepository = new InMemoryUsersRepository()
-  const authenticateUser = new AuthenticateUser(inMemoryUsersRepository);
+  const userRepository = new UserRepository()
+  const authenticateUser = new AuthenticateUser(userRepository);
   const authenticateUserController = new AuthenticateUserController(authenticateUser);
 
   return authenticateUserController;
