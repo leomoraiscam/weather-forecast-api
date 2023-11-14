cc477f6ebdd4d4aa894942c58352c16fd9e1dc10
lmorais <lmorais.contato@gmail.com>
2023-11-14 00:05:49 -0300

    feat: create a new methods in http helper

--- src/shared/http/helpers/http-helper.ts
+++ src/shared/http/helpers/http-helper.ts
@@ -1,6 +1,13 @@
 import { ControllerError } from '@src/shared/errors/ports/controller-error';
 import { HttpResponse } from '../dtos/http-response';
 
+export function ok<T>(data?: T): HttpResponse<T> {
+  return {
+    statusCode: 200,
+    body: data,
+  }
+}
+
 export function created<T>(data: T): HttpResponse<T> {
   return {
     statusCode: 201,
@@ -17,6 +24,17 @@ export function badRequest(data: ControllerError): HttpResponse<ControllerError>
     }
   };
 }
+
+export function forbidden(data: ControllerError): HttpResponse<ControllerError> {
+  return {
+    statusCode: 403,
+    body: {
+      name: data.name,
+      message: data.message
+    }
+  }
+}
+
 
 export function conflict(data: ControllerError): HttpResponse<ControllerError> {
   return {
