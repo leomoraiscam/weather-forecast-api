0288a27594cac455daf75d3bd9bd26bb5e85632f
lmorais <lmorais.contato@gmail.com>
2023-11-19 02:11:09 -0300

    test: add getPositionFromLocation to calc rating for beach

---
+++ src/modules/rating/helpers/get-position-from-location.test.ts
@@ -0,0 +1,42 @@
+import { BeachPosition } from '@config/constants/beach-position-enum';
+import { getPositionFromLocation } from './get-position-from-location';
+
+describe('getPositionFromLocation', () => {
+  const beach = {
+    lat: -33.792726,
+    lng: 151.289824,
+    name: 'Manly',
+    position: BeachPosition.E,
+    userId: 'fake-user-id',
+  };
+
+  it('should be able get the point based on a east location', () => {
+    const rating = getPositionFromLocation(92);
+
+    expect(rating).toBe(BeachPosition.E);
+  });
+
+  it('should be able get the point based on a north location 1', () => {
+    const rating = getPositionFromLocation(360);
+
+    expect(rating).toBe(BeachPosition.N);
+  });
+
+  it('should be able get the point based on a north location 2', () => {
+    const rating = getPositionFromLocation(40);
+
+    expect(rating).toBe(BeachPosition.N);
+  });
+
+  it('should be able get the point based on a south location', () => {
+    const rating = getPositionFromLocation(200);
+
+    expect(rating).toBe(BeachPosition.S);
+  });
+
+  it('should be able get the point based on a west location', () => {
+    const rating = getPositionFromLocation(300);
+
+    expect(rating).toBe(BeachPosition.W);
+  });
+});
