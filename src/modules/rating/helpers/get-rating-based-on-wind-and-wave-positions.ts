2dfe475f5ff26cc9ef93d1b20bc7ae061447159e
lmorais <lmorais.contato@gmail.com>
2023-11-19 01:38:06 -0300

    feat: add getRatingBasedOnWindAndWavePositions to calc rating for beach

---
+++ src/modules/rating/helpers/get-rating-based-on-wind-and-wave-positions.ts
@@ -0,0 +1,32 @@
+import { BeachPosition } from '@config/constants/beach-position-enum';
+
+export function getRatingBasedOnWindAndWavePositions(
+  waveDirection: BeachPosition,
+  windDirection: BeachPosition,
+  beach: any,
+): number {
+  if (waveDirection === windDirection) {
+    return 1;
+  } else if (isWindOffShore(beach, waveDirection, windDirection)) {
+    return 5;
+  }
+
+  return 3;
+}
+
+function isWindOffShore(beach: any, waveDirection: string, windDirection: string): boolean {
+  return (
+    (waveDirection === BeachPosition.N &&
+      windDirection === BeachPosition.S &&
+      beach.position === BeachPosition.N) ||
+    (waveDirection === BeachPosition.S &&
+      windDirection === BeachPosition.N &&
+      beach.position === BeachPosition.S) ||
+    (waveDirection === BeachPosition.E &&
+      windDirection === BeachPosition.W &&
+      beach.position === BeachPosition.E) ||
+    (waveDirection === BeachPosition.W &&
+      windDirection === BeachPosition.E &&
+      beach.position === BeachPosition.W)
+  );
+}
