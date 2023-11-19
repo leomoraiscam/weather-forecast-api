083d571822b3804e4005e51bccd4f7e5984c6c41
lmorais <lmorais.contato@gmail.com>
2023-11-19 02:10:57 -0300

    test: add getPositionFromLocation to calc rating for beach

---
+++ src/modules/rating/helpers/get-position-from-location.ts
@@ -0,0 +1,13 @@
+import { BeachPosition } from '@config/constants/beach-position-enum';
+
+export function getPositionFromLocation(coordinates: number): BeachPosition {
+  if (coordinates < 50) return BeachPosition.N;
+
+  if (coordinates < 120) return BeachPosition.E;
+
+  if (coordinates < 220) return BeachPosition.S;
+
+  if (coordinates < 310) return BeachPosition.W;
+
+  return BeachPosition.N;
+}
