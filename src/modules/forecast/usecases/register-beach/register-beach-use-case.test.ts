035df07951979fa11e7b566ff07cf41460199e89
lmorais <lmorais.contato@gmail.com>
2023-11-25 15:51:28 -0300

    refactor: use beach factory in register beach test

--- src/modules/forecast/usecases/register-beach/register-beach-use-case.test.ts
+++ src/modules/forecast/usecases/register-beach/register-beach-use-case.test.ts
@@ -1,40 +1,40 @@
 import { BeachPosition } from '@config/constants/beach-position-enum';
+import { createBeach } from '@test/factories/BeachFactory';
 
-import { Beach } from '../../domain/beach/beach';
-import { Latitude } from '../../domain/beach/latitude';
-import { Longitude } from '../../domain/beach/longitude';
-import { Name } from '../../domain/beach/name';
-import { Position } from '../../domain/beach/position';
+import { IBeach } from '../../dtos/beach';
 import { IBeachRepository } from '../../repositories/beaches-repository';
 import { InMemoryBeachRepository } from '../../repositories/in-memory/in-memory-beach-repository';
 import { RegisterBeachUseCase } from './register-beach-use-case';
 
 let beachRepository: IBeachRepository;
 let registerBeachUseCase: RegisterBeachUseCase;
+let beach: IBeach;
 
 describe('Create Beach Use Case', () => {
   beforeEach(() => {
     beachRepository = new InMemoryBeachRepository();
     registerBeachUseCase = new RegisterBeachUseCase(beachRepository);
+
+    beach = {
+      name: 'Dee why',
+      lat: -33.792726,
+      lng: 151.289824,
+      position: BeachPosition.E,
+      userId: 'fake-user-id',
+    };
   });
 
   it('should be able to register new beach', async () => {
-    const response = await registerBeachUseCase.execute({
-      name: 'John Doe',
-      lat: 1,
-      lng: 1,
-      position: BeachPosition.E,
-      userId: 'fake-user-id',
-    });
+    const response = await registerBeachUseCase.execute(beach);
 
     expect(response.isRight()).toBeTruthy();
   });
 
   it('should not be able to register new beach with invalid data', async () => {
     const response = await registerBeachUseCase.execute({
-      name: 'John Doe',
+      name: 'Dee why',
       lat: 151.289824,
-      lng: 1,
+      lng: 151.289824,
       position: BeachPosition.E,
       userId: 'fake-user-id',
     });
@@ -42,29 +42,12 @@ describe('Create Beach Use Case', () => {
     expect(response.isLeft()).toBeTruthy();
   });
 
-  it('should not be able to register new beach with existing lat and lng', async () => {
-    const name = Name.create('Dee Why').value as Name;
-    const lat = Latitude.create(-33.792726).value as Latitude;
-    const lng = Longitude.create(151.289824).value as Longitude;
-    const position = Position.create(BeachPosition.S).value as Position;
-
-    const beachOrError = Beach.create({
-      name,
-      lat,
-      lng,
-      position,
-      userId: 'fake-user-id',
-    }).value as Beach;
+  it('should not be able to register new beach with existing lat and lng to the same user', async () => {
+    const beachOrError = createBeach();
 
     beachRepository.create(beachOrError);
 
-    const response = await registerBeachUseCase.execute({
-      name: 'John Doe',
-      lat: -33.792726,
-      lng: 151.289824,
-      position: BeachPosition.S,
-      userId: 'fake-user-id',
-    });
+    const response = await registerBeachUseCase.execute(beach);
 
     expect(response.isLeft()).toBeTruthy();
   });
