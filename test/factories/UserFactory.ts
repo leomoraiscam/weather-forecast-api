9634e48bddaef94e3adf9628e23a1a84bf72c09f
lmorais <lmorais.contato@gmail.com>
2023-11-25 11:01:14 -0300

    feat: create user factory to use in tests

---
+++ test/factories/UserFactory.ts
@@ -0,0 +1,23 @@
+import { Email } from '../../src/modules/accounts/domain/user/email';
+import { Name } from '../../src/modules/accounts/domain/user/name';
+import { Password } from '../../src/modules/accounts/domain/user/password';
+import { User } from '../../src/modules/accounts/domain/user/user';
+
+type UserOverrides = {
+  email?: string;
+  password?: string;
+};
+
+export function createUser(overrides?: UserOverrides) {
+  const name = Name.create('John Doe').value as Name;
+  const email = Email.create(overrides?.email ?? 'john@doe.com').value as Email;
+  const password = Password.create(overrides?.password ?? '123456').value as Password;
+
+  const user = User.create({
+    name,
+    email,
+    password,
+  });
+
+  return user.value as User;
+}
