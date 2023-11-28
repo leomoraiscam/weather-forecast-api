935832e1c2c21c6b5c24b3988fa0a11854c48b7b
lmorais <lmorais.contato@gmail.com>
2023-11-28 14:40:40 -0300

    feat: create cache provider implementation

---
+++ src/external/cache-service/implementations/redis-cache-provider.ts
@@ -0,0 +1,27 @@
+import Redis, { Redis as RedisClient } from 'ioredis';
+
+import cacheConfig from '@src/external/database/redis/config/cache';
+
+import { ICacheProvider } from '../models/cache-provider';
+
+export class RedisCacheProvider implements ICacheProvider {
+  private client: RedisClient;
+
+  constructor() {
+    this.client = new Redis(cacheConfig.config.redis);
+  }
+
+  async save(key: string, value: any): Promise<void> {
+    await this.client.set(key, JSON.stringify(value));
+  }
+
+  async recover<T>(key: string): Promise<T> {
+    const data = await this.client.get(key);
+
+    if (!data) {
+      return null;
+    }
+
+    return JSON.parse(data) as T;
+  }
+}
