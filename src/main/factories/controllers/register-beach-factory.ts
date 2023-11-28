8ea5dfda6b8a6dd3d4efbfa203085b603aa3824d
lmorais <lmorais.contato@gmail.com>
2023-11-28 14:06:29 -0300

    feat: apply validator in register user and register beach controller

--- src/main/factories/controllers/register-beach-factory.ts
+++ src/main/factories/controllers/register-beach-factory.ts
@@ -1,13 +1,17 @@
 import { BeachRepository } from '@src/modules/forecast/repositories/implementations/beach-repository';
 import { RegisterBeachController } from '@src/modules/forecast/usecases/register-beach/register-beach-controller';
 import { RegisterBeachUseCase } from '@src/modules/forecast/usecases/register-beach/register-beach-use-case';
+import { RequiredFieldsValidator } from '@src/shared/logic/validators/required-fields-validator';
 
 import { IController } from '../../adapters/ports/controller';
 
 export const makeRegisterBeachController = (): IController => {
   const beachRepository = new BeachRepository();
   const registerBeachUseCase = new RegisterBeachUseCase(beachRepository);
-  const registerBeachController = new RegisterBeachController(registerBeachUseCase);
+
+  const validator = new RequiredFieldsValidator();
+
+  const registerBeachController = new RegisterBeachController(registerBeachUseCase, validator);
 
   return registerBeachController;
 };
--- src/main/factories/controllers/register-user-factory.ts
+++ src/main/factories/controllers/register-user-factory.ts
@@ -1,13 +1,17 @@
 import { UserRepository } from '@src/modules/accounts/repositories/implementations/users-repository';
 import { RegisterUserController } from '@src/modules/accounts/usecases/register-user/register-user-controller';
 import { RegisterUserUseCase } from '@src/modules/accounts/usecases/register-user/register-user-use-case';
+import { RequiredFieldsValidator } from '@src/shared/logic/validators/required-fields-validator';
 
 import { IController } from '../../adapters/ports/controller';
 
 export const makeRegisterUserController = (): IController => {
   const userRepository = new UserRepository();
   const registerUserUseCase = new RegisterUserUseCase(userRepository);
-  const registerUserController = new RegisterUserController(registerUserUseCase);
+
+  const validator = new RequiredFieldsValidator();
+
+  const registerUserController = new RegisterUserController(registerUserUseCase, validator);
 
   return registerUserController;
 };
