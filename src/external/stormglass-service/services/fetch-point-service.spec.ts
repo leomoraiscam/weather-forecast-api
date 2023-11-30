fc8eac8d44d963d60e41ddbeeeb4790abc81d720
lmorais <lmorais.contato@gmail.com>
2023-11-30 15:14:14 -0300

    feat: implementation cache provider in memory

--- src/external/stormglass-service/services/fetch-point-service.spec.ts
+++ src/external/stormglass-service/services/fetch-point-service.spec.ts
@@ -1,3 +1,4 @@
+import { InMemoryCacheProvider } from '@src/external/cache-service/in-memory/in-memory-cache-provider';
 import { AxiosRequestProvider } from '@src/external/stormglass-service/providers/implementations/axios-request-provider';
 import fetchPointsNormalizedResponse from '@test/fixtures/fetch-points-normalized-response.json';
 import stormGlassIncompleteResponse from '@test/fixtures/storm-glass-incomplete-response.json';
@@ -8,6 +9,8 @@ import stormGlassWeather3HoursResponse from '@test/fixtures/storm-glass-response
 import { FetchPointService } from './fetch-point-service';
 
 jest.mock('@src/external/stormglass-service/providers/implementations/axios-request-provider');
+
+let inMemoryCacheProvider: InMemoryCacheProvider;
 
 describe('StormGlass Service', () => {
   const mockedRequest = new AxiosRequestProvider() as jest.Mocked<AxiosRequestProvider>;
@@ -23,12 +26,14 @@ describe('StormGlass Service', () => {
 
     lat = inputGeolocation.lat;
     lng = inputGeolocation.lng;
+
+    inMemoryCacheProvider = new InMemoryCacheProvider();
   });
 
   it('should be able to return the normalize forecast data from the stormGlass service', async () => {
     mockedRequest.get.mockResolvedValue({ data: stormGlassWeather3HoursResponse, status: 200 });
 
-    const fetchPointService = new FetchPointService(mockedRequest);
+    const fetchPointService = new FetchPointService(mockedRequest, inMemoryCacheProvider);
 
     const response = await fetchPointService.execute({ lat, lng });
 
@@ -40,7 +45,7 @@ describe('StormGlass Service', () => {
   it('should be able to exclude incomplete data points received from stormGlass service', async () => {
     mockedRequest.get.mockResolvedValue({ data: stormGlassIncompleteResponse, status: 200 });
 
-    const fetchPointService = new FetchPointService(mockedRequest);
+    const fetchPointService = new FetchPointService(mockedRequest, inMemoryCacheProvider);
 
     const response = await fetchPointService.execute({ lat, lng });
 
@@ -52,7 +57,7 @@ describe('StormGlass Service', () => {
   it('should be able to get a generic error from stormGlass service when the request fail before reaching the service', async () => {
     mockedRequest.get.mockRejectedValue(stormGlassResponseGenericError);
 
-    const fetchPointService = new FetchPointService(mockedRequest);
+    const fetchPointService = new FetchPointService(mockedRequest, inMemoryCacheProvider);
 
     const response = await fetchPointService.execute({ lat, lng });
 
@@ -62,7 +67,7 @@ describe('StormGlass Service', () => {
   it('should be able to get an StormGlassResponseError when the StormGlass service responds with error', async () => {
     mockedRequest.get.mockRejectedValue(stormGlassResponseRateLimitError);
 
-    const fetchPointService = new FetchPointService(mockedRequest);
+    const fetchPointService = new FetchPointService(mockedRequest, inMemoryCacheProvider);
 
     const response = await fetchPointService.execute({ lat, lng });
 
