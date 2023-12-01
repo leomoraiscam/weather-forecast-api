275e7a61238c10a1a9d64f741d622bde8b453dd7
lmorais <lmorais.contato@gmail.com>
2023-12-01 15:17:06 -0300

    refactor: improve ensure auth middleware

--- src/shared/http/middlewares/ensure-authenticated-middleware.ts
+++ src/shared/http/middlewares/ensure-authenticated-middleware.ts
@@ -1,8 +1,8 @@
-/* eslint-disable no-useless-constructor */
 /* eslint-disable @typescript-eslint/no-namespace */
 import { decode } from 'jsonwebtoken';
 
 import { IMiddleware } from '@src/main/adapters/ports/middleware';
+import { InvalidJWTTokenError } from '@src/modules/accounts/domain/user/errors/invalid-jwt-token-error';
 import { IControllerError } from '@src/shared/errors/ports/controller-error';
 
 import {
@@ -10,7 +10,7 @@ import {
   IDecodedJwt,
 } from '../dtos/ensure-authenticated-request';
 import { IHttpResponse } from '../dtos/http-response';
-import { ok, forbidden, serverError } from '../helpers/http-helper';
+import { ok, unauthorized } from '../helpers/http-helper';
 
 export class EnsureAuthenticatedMiddleware implements IMiddleware {
   constructor() {}
@@ -18,28 +18,18 @@ export class EnsureAuthenticatedMiddleware implements IMiddleware {
   async handle(
     request: IEnsureAuthenticatedMiddlewareRequest,
   ): Promise<IHttpResponse<{ userId: string } | IControllerError>> {
+    const { accesstoken } = request;
+
+    if (!accesstoken) {
+      return unauthorized(new InvalidJWTTokenError());
+    }
+
     try {
-      const { accesstoken } = request;
+      const decoded = decode(accesstoken) as IDecodedJwt;
 
-      if (accesstoken) {
-        try {
-          const decoded = decode(accesstoken) as IDecodedJwt;
-
-          return ok({ userId: decoded.sub });
-        } catch (err) {
-          return forbidden({
-            name: 'AccessDeniedError',
-            message: 'Access denied.',
-          });
-        }
-      }
-
-      return forbidden({
-        name: 'AccessDeniedError',
-        message: 'Access denied.',
-      });
+      return ok({ userId: decoded.sub });
     } catch (error) {
-      return serverError(error);
+      return unauthorized(new InvalidJWTTokenError());
     }
   }
 }
