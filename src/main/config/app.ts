f573767659c5183463d877f010791493c5b2e211
lmorais <lmorais.contato@gmail.com>
2023-12-23 00:03:19 -0300

    feat: create server main file

---
+++ src/main/config/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerFile from '../../shared/swagger.json';
+import { bodyParser } from './body-parser';
+import { contentType } from './content-type';
+import { cors } from './cors';
+import setupRoute from './routes';
+
+const app = express();
+
+app.use(bodyParser);
+app.use(cors);
+app.use(contentType);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+setupRoute(app);
+
+export default app;
---
+++ src/main/server.ts
@@ -0,0 +1,19 @@
+import '../shared/module-alias';
+import * as dotenv from 'dotenv';
+
+import { mongoHelper } from '@src/external/database/mongodb/helpers/mongo-helper';
+
+dotenv.config();
+
+mongoHelper
+  .connect(`mongodb://${process.env.MONGO_URL}:${process.env.MONGO_PORT}/`)
+  .then(async () => {
+    const app = (await import('./config/app')).default;
+
+    app.listen(process.env.APP_PORT || 3000, () => {
+      console.log(`Server running at http://localhost:${process.env.APP_PORT || 3000}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Error: ${error}`);
+  });
