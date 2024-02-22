b57afecf3b85ee119286817cdc5d3662b3e8c9ec
lmorais <lmorais.contato@gmail.com>
2024-02-21 23:41:08 -0300

    test: rename and improve authenticate user use case test

--- src/modules/accounts/usecases/authenticate-user/authenticate-user-use-case.spec.ts
+++ src/modules/accounts/usecases/authenticate-user/authenticate-user-use-case.spec.ts
@@ -2,32 +2,37 @@ import { createUser } from '@test/factories/user-factory';
 
 import { InMemoryUserRepository } from '../../repositories/in-memory/in-memory-users-repository';
 import { IUserRepository } from '../../repositories/user-repository';
-import { AuthenticateUser } from './authenticate-user';
+import { AuthenticateUserUseCase } from './authenticate-user-use-case';
 
 let usersRepository: IUserRepository;
-let authenticateUser: AuthenticateUser;
+let authenticateUserUseCase: AuthenticateUserUseCase;
 
 describe('Authenticate User Use case', () => {
   beforeEach(() => {
     usersRepository = new InMemoryUserRepository();
-    authenticateUser = new AuthenticateUser(usersRepository);
+    authenticateUserUseCase = new AuthenticateUserUseCase(usersRepository);
   });
 
-  it('should be able to authenticate', async () => {
+  it('should be able return token property to user when the same is authenticate with success', async () => {
     const user = createUser();
 
     usersRepository.create(user);
 
-    const response = await authenticateUser.execute({
+    const response = await authenticateUserUseCase.execute({
       email: 'john@doe.com',
       password: '123456',
     });
 
+    expect(response.value).toHaveProperty('token');
     expect(response.value).toEqual(expect.objectContaining({ token: expect.any(String) }));
   });
 
-  it('should not be able to authenticate with invalid e-mail', async () => {
-    const response = await authenticateUser.execute({
+  it('should not be able return token property to user when received incorrect email for existing user', async () => {
+    const user = createUser();
+
+    usersRepository.create(user);
+
+    const response = await authenticateUserUseCase.execute({
       email: 'invalid@example.com',
       password: '123456',
     });
@@ -35,12 +40,12 @@ describe('Authenticate User Use case', () => {
     expect(response.isLeft()).toBeTruthy();
   });
 
-  it('should not be able to authenticate with invalid password', async () => {
+  it('should not be able return token property to user when received incorrect password for existing user', async () => {
     const user = createUser();
 
     usersRepository.create(user);
 
-    const response = await authenticateUser.execute({
+    const response = await authenticateUserUseCase.execute({
       email: 'john@doe.com',
       password: '123456789',
     });
