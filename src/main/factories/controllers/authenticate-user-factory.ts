04ea7a648e8eb081f826f303115083bb3ec191d0
lmorais <lmorais.contato@gmail.com>
2024-02-22 12:12:41 -0300

    fix: resolve wrong path in factory

--- src/main/factories/controllers/authenticate-user-factory.ts
+++ src/main/factories/controllers/authenticate-user-factory.ts
@@ -1,13 +1,13 @@
 import { UserRepository } from '@src/modules/accounts/repositories/implementations/users-repository';
-import { AuthenticateUser } from '@src/modules/accounts/usecases/authenticate-user/authenticate-user';
 import { AuthenticateUserController } from '@src/modules/accounts/usecases/authenticate-user/authenticate-user-controller';
+import { AuthenticateUserUseCase } from '@src/modules/accounts/usecases/authenticate-user/authenticate-user-use-case';
 import { RequiredFieldsValidator } from '@src/shared/logic/validators/required-fields-validator';
 
 import { IController } from '../../adapters/ports/controller';
 
 export const makeAuthenticateUserController = (): IController => {
   const userRepository = new UserRepository();
-  const authenticateUser = new AuthenticateUser(userRepository);
+  const authenticateUser = new AuthenticateUserUseCase(userRepository);
 
   const validator = new RequiredFieldsValidator();
 
