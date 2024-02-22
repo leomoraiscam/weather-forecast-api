5950e6b11ea1fb993ba6a0a2c912bdbae7d43570
lmorais <lmorais.contato@gmail.com>
2024-02-21 23:43:24 -0300

    refactor: improve user controller

--- src/modules/accounts/usecases/authenticate-user/authenticate-user-controller.ts
+++ src/modules/accounts/usecases/authenticate-user/authenticate-user-controller.ts
@@ -3,30 +3,25 @@ import { IValidator } from '@src/main/adapters/ports/validator';
 import { IControllerError } from '@src/shared/errors/ports/controller-error';
 import { IHttpRequest } from '@src/shared/http/dtos/http-request';
 import { IHttpResponse } from '@src/shared/http/dtos/http-response';
-import {
-  badRequest,
-  created,
-  serverError,
-  unauthorized,
-} from '@src/shared/http/helpers/http-helper';
+import { badRequest, ok, serverError, unauthorized } from '@src/shared/http/helpers/http-helper';
 
-import { IAuthenticateUserRequest } from '../../dtos/authenticate-user-request';
-import { IAuthenticateUserResponse } from '../../dtos/authenticate-user-response';
+import { IAuthenticateUserDTO } from '../../dtos/authenticate-user';
+import { IAuthenticationUserDTO } from '../../dtos/authentication-user';
 import { InvalidEmailOrPasswordError } from './errors/invalid-email-or-password-error';
 
 export class AuthenticateUserController {
   private readonly usecase: IUseCase;
-  private readonly validator: IValidator<IAuthenticateUserRequest>;
+  private readonly validator: IValidator<IAuthenticateUserDTO>;
   readonly requiredParams = ['email', 'password'];
 
-  constructor(usecase: IUseCase, validator: IValidator<IAuthenticateUserRequest>) {
+  constructor(usecase: IUseCase, validator: IValidator<IAuthenticateUserDTO>) {
     this.usecase = usecase;
     this.validator = validator;
   }
 
   async handle(
-    request: IHttpRequest<IAuthenticateUserRequest>,
-  ): Promise<IHttpResponse<IAuthenticateUserResponse | IControllerError>> {
+    request: IHttpRequest<IAuthenticateUserDTO>,
+  ): Promise<IHttpResponse<IAuthenticationUserDTO | IControllerError>> {
     const validator = this.validator.validate(request.body, this.requiredParams);
 
     if (validator.isLeft()) {
@@ -47,7 +42,7 @@ export class AuthenticateUserController {
         }
       }
 
-      return created<IAuthenticateUserResponse>(response.value);
+      return ok<IAuthenticationUserDTO>(response.value);
     } catch (error) {
       return serverError(error);
     }
