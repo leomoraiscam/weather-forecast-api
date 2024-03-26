fbbfcfb6431c2d7a4f70267c82a63d028e6a78f3
lmorais <lmorais.contato@gmail.com>
2024-03-25 22:48:16 -0300

    refactor: make key and value to build object array

--- src/modules/forecast/helpers/normalize-forecast-by-time.ts
+++ src/modules/forecast/helpers/normalize-forecast-by-time.ts
@@ -1,24 +1,18 @@
-/* eslint-disable no-restricted-syntax */
 import { IBeachRatingForecastDTO } from '../dtos/beach-rating-forecast';
 import { ITimeBeachRatingForecastDTO } from '../dtos/time-beach-rating-forecast';
 
 export function normalizeForecastByTime(
   forecast: IBeachRatingForecastDTO[],
 ): ITimeBeachRatingForecastDTO[] {
-  const forecastByTime: ITimeBeachRatingForecastDTO[] = [];
+  const forecastByTime: { [time: string]: ITimeBeachRatingForecastDTO } = {};
 
   for (const point of forecast) {
-    const timePoint = forecastByTime.find((forecastData) => forecastData.time === point.time);
-
-    if (timePoint) {
-      timePoint.forecast.push(point);
-    } else {
-      forecastByTime.push({
-        time: point.time,
-        forecast: [point],
-      });
+    if (!forecastByTime[point.time]) {
+      forecastByTime[point.time] = { time: point.time, forecast: [] };
     }
+
+    forecastByTime[point.time].forecast.push(point);
   }
 
-  return forecastByTime;
+  return Object.values(forecastByTime);
 }
