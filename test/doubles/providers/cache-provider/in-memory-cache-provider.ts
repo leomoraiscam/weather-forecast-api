9d6c159f8c06c8c4608f1f86d12832aaf9de0cd9
lmorais <lmorais.contato@gmail.com>
2024-11-30 13:14:43 -0300

    feat: implementation a new faked object cache provider

---
+++ test/doubles/providers/cache-provider/in-memory-cache-provider.ts
@@ -0,0 +1,29 @@
+import { ICacheProvider, ICacheData } from '@src/application/interfaces/providers/cache-provider';
+
+export class InMemoryCacheProvider implements ICacheProvider {
+  private cache: ICacheData = {};
+
+  public async save<T>(key: string, value: T): Promise<void> {
+    this.cache[key] = JSON.stringify(value);
+  }
+
+  public async recover<T>(key: string): Promise<T | null> {
+    const data = this.cache[key];
+
+    if (!data) {
+      return null;
+    }
+
+    const parsedData = JSON.parse(data) as T;
+
+    return parsedData;
+  }
+
+  public async invalidatePrefix(prefix: string): Promise<void> {
+    const keys = Object.keys(this.cache).filter((key) => key.startsWith(`${prefix}:`));
+
+    keys.forEach((key) => {
+      delete this.cache[key];
+    });
+  }
+}
