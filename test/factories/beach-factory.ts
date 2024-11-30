e9babda0d978ea0682e05ee72c021733fff3d6bc
lmorais <lmorais.contato@gmail.com>
2024-11-30 11:44:57 -0300

    test: update beach depedencies in beach factory

--- test/factories/beach-factory.ts
+++ test/factories/beach-factory.ts
@@ -1,9 +1,9 @@
 import { BeachPosition } from '@src/config/constants/beach-position-enum';
-import { Beach } from '@src/modules/forecast/domain/beach/beach';
-import { Latitude } from '@src/modules/forecast/domain/beach/latitude';
-import { Longitude } from '@src/modules/forecast/domain/beach/longitude';
-import { Name } from '@src/modules/forecast/domain/beach/name';
-import { Position } from '@src/modules/forecast/domain/beach/position';
+import { Beach } from '@src/entities/beach/beach';
+import { Latitude } from '@src/entities/beach/latitude';
+import { Longitude } from '@src/entities/beach/longitude';
+import { Name } from '@src/entities/beach/name';
+import { Position } from '@src/entities/beach/position';
 
 type BeachOverrides = {
   name?: string;
