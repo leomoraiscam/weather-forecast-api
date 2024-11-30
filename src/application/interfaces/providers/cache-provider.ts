54c14f9ffc1f68780fe17c857449ec310b127071
lmorais <lmorais.contato@gmail.com>
2024-11-30 11:46:46 -0300

    feat: implementation a new abstract cache provider contract

---
+++ src/application/interfaces/providers/cache-provider.ts
@@ -0,0 +1,9 @@
+export interface ICacheData {
+  [key: string]: string;
+}
+
+export interface ICacheProvider {
+  save<T>(key: string, value: T): Promise<void>;
+  recover<T>(key: string): Promise<T | null>;
+  invalidatePrefix(prefix: string): Promise<void>;
+}
