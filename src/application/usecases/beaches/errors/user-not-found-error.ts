21ab61571f4ba3ede0e417eb1916a3febdbfcdc0
lmorais <lmorais.contato@gmail.com>
2024-11-30 11:49:33 -0300

    feat: add user not found custom error

---
+++ src/application/usecases/beaches/errors/user-not-found-error.ts
@@ -0,0 +1,8 @@
+import { IUseCaseError } from '@src/shared/errors/ports/use-case-error';
+
+export class UserNotFoundError extends Error implements IUseCaseError {
+  constructor() {
+    super(`The user does not exist.`);
+    this.name = 'UserNotFoundError';
+  }
+}
