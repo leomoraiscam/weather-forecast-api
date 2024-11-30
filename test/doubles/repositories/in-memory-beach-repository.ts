1441c7b24fd5be1d2ca9dc891748a679d6d1d32d
lmorais <lmorais.contato@gmail.com>
2024-11-30 13:15:02 -0300

    feat: implementation a new faked object beach repository

---
+++ test/doubles/repositories/in-memory-beach-repository.ts
@@ -0,0 +1,37 @@
+import { IBeachRepository } from '@src/application/interfaces/repositories/beach-repository';
+import { Beach } from '@src/entities/beach/beach';
+import { IBeachCoordinatesDTO } from '@src/modules/forecast/dtos/beach-coordinates';
+import { BeachMapper } from '@src/modules/forecast/mapper/beach-mapper';
+
+export class InMemoryBeachRepository implements IBeachRepository {
+  beaches: Beach[] = [];
+
+  async findByGeolocation(data: IBeachCoordinatesDTO): Promise<Beach> {
+    const { lat, lng, userId } = data;
+
+    return this.beaches.find(
+      (beach) => beach.lat.value === lat && beach.lng.value === lng && beach.userId === userId,
+    );
+  }
+
+  async findAllBeachesByUser(userId: string): Promise<Beach[]> {
+    const beaches = this.beaches.filter((beach) => beach.userId === userId);
+
+    const serializerBeach = beaches.map((beach) => ({
+      id: beach.id,
+      name: beach.name.value,
+      lat: beach.lat.value,
+      lng: beach.lng.value,
+      position: beach.position.value,
+      userId: beach.userId,
+    }));
+
+    return BeachMapper.toDomain(serializerBeach) as any;
+  }
+
+  async create(beach: Beach): Promise<Beach> {
+    this.beaches.push(beach);
+
+    return beach;
+  }
+}
