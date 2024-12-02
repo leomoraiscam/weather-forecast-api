f6e1d968282f6175d31cd1074af05be622d1a2bc
lmorais <lmorais.contato@gmail.com>
2024-12-01 23:20:42 -0300

    refactor: use a new implementations in user beaches processing forecast factory

--- src/main/factories/controllers/register-beach-factory.ts
+++ src/main/factories/controllers/register-beach-factory.ts
@@ -1,14 +1,14 @@
 import { RegisterBeachUseCase } from '@src/application/usecases/beaches/register-beach/register-beach-use-case';
 import { BeachRepository } from '@src/infrastructure/database/mongo/repositories/beaches/beach-repository';
 import { UserRepository } from '@src/infrastructure/database/mongo/repositories/users/user-repository';
-import { RedisCacheService } from '@src/infrastructure/providers/cache-provider/redis-cache-provider';
+import { RedisCacheProvider } from '@src/infrastructure/providers/cache-provider/redis-cache-provider';
 import { IController } from '@src/main/adapters/ports/controller';
 import { RegisterBeachController } from '@src/presentation/controllers/register-beach-controller';
 
 export const makeRegisterBeachController = (): IController => {
   const beachRepository = new BeachRepository();
   const userRepository = new UserRepository();
-  const cacheService = new RedisCacheService();
+  const cacheService = new RedisCacheProvider();
   const registerBeachUseCase = new RegisterBeachUseCase(
     beachRepository,
     userRepository,
