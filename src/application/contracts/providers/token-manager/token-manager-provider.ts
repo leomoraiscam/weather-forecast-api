036d53e564da4f910387cba64dcea0eb010ba3a9
lmorais <lmorais.contato@gmail.com>
2024-12-12 22:38:14 -0300

    feat: add verify method in contract interface and token manager implementation

--- src/application/contracts/providers/token-manager/token-manager-provider.ts
+++ src/application/contracts/providers/token-manager/token-manager-provider.ts
@@ -2,4 +2,5 @@ import { JWTPayload } from './dtos/token-data';
 
 export interface ITokenManagerProvider {
   sign(payload: JWTPayload, expiresIn?: string): Promise<string>;
+  verify(token: string): Promise<JWTPayload>;
 }
--- src/infrastructure/providers/token-manager/jwt-token-manager-provider.ts
+++ src/infrastructure/providers/token-manager/jwt-token-manager-provider.ts
@@ -1,4 +1,4 @@
-import { sign } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
 
 import { JWTPayload } from '@src/application/contracts/providers/token-manager/dtos/token-data';
 import { ITokenManagerProvider } from '@src/application/contracts/providers/token-manager/token-manager-provider';
@@ -8,5 +8,9 @@ export class JWTTokenManagerProvider implements ITokenManagerProvider {
     return sign(payload, process.env.JWT_SECRET_TOKEN, {
       expiresIn: expiresIn || process.env.JWT_EXPIRES_IN,
     });
+  }
+
+  async verify(token: string): Promise<JWTPayload> {
+    return verify(token, process.env.JWT_SECRET_TOKEN) as JWTPayload;
   }
 }
