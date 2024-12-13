501050ed5fdc13daf8ad5942d38acb05a5bca599
lmorais <lmorais.contato@gmail.com>
2024-12-12 22:41:45 -0300

    feat: implementation a new rate limiter middleware in presentation layer

---
+++ src/presentation/middlewares/rate-limiter-middleware.ts
@@ -0,0 +1,26 @@
+import { IRateLimiter } from '@src/application/contracts/providers/rate-limiter/rate-limiter-provider';
+
+import { IHttpResponse } from '../contracts/http-response';
+import { IMiddleware } from '../contracts/middleware';
+import { TooManyRequestsError } from '../errors/too-many-requests-error';
+import { toManyRequests, ok } from '../helpers/http-helper';
+
+type RateLimiterMiddlewareRequest = {
+  ip: string;
+};
+
+export class RateLimiterMiddleware implements IMiddleware {
+  constructor(private rateLimiter: IRateLimiter) {}
+
+  async handle(request: RateLimiterMiddlewareRequest): Promise<IHttpResponse> {
+    try {
+      const { ip } = request;
+
+      await this.rateLimiter.consume(ip);
+
+      return ok({ success: true });
+    } catch (error) {
+      return toManyRequests(new TooManyRequestsError());
+    }
+  }
+}
